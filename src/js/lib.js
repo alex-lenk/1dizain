8a46dedc59b555a9379d730795c6a07687f69d30
artlenk.ru@gmail.com <df5ukoi7ht>
2018-05-02 14:54:19 +0300

    Initial commit

---
+++ src/js/lib.js
@@ -0,0 +1,25 @@
+/*
+ *
+ * Content of libraries
+ * 1.   smoothscroll.js
+ * 2.   svgxuse.min.js
+ * 3.   slick.min.js
+ * 4.   stickUp.min.js
+ * 5.   bootstrap.min.js
+ *
+ */
+
+/* 1. smoothscroll.js */
+//= lib/smoothscroll.js
+
+/* 2. svgxuse.min.js */
+// = lib/svgxuse.min.js
+
+/* 3. slick.min.js */
+// = ../../bower_components/slick-carousel/slick/slick.min.js
+
+/* 4. stickUp.min.js */
+// = lib/stickUp.min.js
+
+/* 5. bootstrap.min.js */
+// = lib/bootstrap.min.js
